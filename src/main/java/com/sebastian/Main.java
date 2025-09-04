98e9697d539f3af51334f12949dfd21b86c2551c
juanse217 <juansebas08004@gmail.com>
2025-09-04 04:24:16 -0500

    chore: actualizacion pom para incluir logs

--- src/main/java/com/sebastian/Main.java
+++ src/main/java/com/sebastian/Main.java
@@ -1,7 +1,32 @@
 package com.sebastian;
 
+import com.sebastian.model.Book;
+import com.sebastian.model.BookNotFoundException;
+import com.sebastian.model.User;
+import com.sebastian.repository.MemoryBookRepository;
+import com.sebastian.repository.MemoryLoanRepository;
+import com.sebastian.service.LibraryService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LibraryService service = new LibraryService(new MemoryBookRepository(), new MemoryLoanRepository());
+
+        Book book = new Book(1, "Noche estrella", "Juan");
+        Book book1 = new Book(2, "El llano en llamas", "Juan Rulfo");
+        Book book2 = new Book(3, "El jugador", "Dostoyevski");
+
+        service.addBook(book);
+        service.addBook(book1);
+        service.addBook(book2);
+
+        try {
+            System.out.println(service.getBook(1).getTitle());
+            System.out.println(service.getBook(4));
+        } catch (BookNotFoundException e) {
+            System.out.println("error");
+        }
+
+        service.loanBook(new User(4, "Selbastian"), book2);
+        
     }
 }
